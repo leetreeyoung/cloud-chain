95bd66576b0c4d20ede8e5570063fa7a8e5c239f
leetreeyoung <yangzhen0119_sjtu@alumni.sjtu.edu.cn>
2020-12-10 23:17:54 +0800

    add internal

--- cloudchain-backend/controllers/controller.go
+++ cloudchain-backend/controllers/controller.go
@@ -1,3 +1,29 @@
 package controllers
 
+import (
+	"cloudchain-backend/models/dto"
+	"encoding/json"
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+)
 
+type Controller struct {
+	beego.Controller
+}
+
+func (c *Controller) Get() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "index.tpl"
+}
+
+func (c *Controller) TestModel() {
+	var testModel dto.TestModel
+	err := json.Unmarshal(c.Ctx.Input.RequestBody,&testModel)
+	if err != nil {
+		logs.Error(err)
+	}
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "index.tpl"
+}
--- cloudchain-backend/internal/internal.go
+++ cloudchain-backend/internal/internal.go
@@ -1,7 +1,7 @@
 package internal
 
 type internal struct {
-	
+
 
 }
 
